5c0ff5ceb040f2d10754ac377d95d954d898c5d5
MinJae Lee <wjdchs0129@gnu.ac.kr>
2023-01-15 23:00:35 +0900

    .

--- main.py
+++ main.py
@@ -9,30 +9,36 @@ def main():
 
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
-    if request.method == 'POST': 
-        req_user_name, req_password = request.form['user_name'].strip(), request.form['password'].strip()    
-        if req_user_name not in UserReader.getUserAccountsDB():
-            UserWriter.writeNewDirectoryAndFiles(req_user_name, req_password)
-            return render_template('success_signup.html')
-        return render_template('retry_signup.html')
-    return render_template('signup.html')
+    if request.method == 'GET': return render_template('signup.html')
+    
+    req_user_name, req_password = request.form['user_name'].strip(), request.form['password'].strip()    
+    if req_user_name not in UserReader.getUserAccountsDB():
+        UserWriter.writeNewDirectoryAndFiles(req_user_name, req_password)
+        return redirect(url_for('success', action='login'))
+    return redirect(url_for('fail', action='signup'))
 
-@app.route('/analyze', methods=['POST'])
-def analyze():
-    req_user_name = request.form['user_name'].strip()
-    try:
-        if UserReader.getUserAccountsDB()[req_user_name] == request.form['password'].strip():
-            full_of_follow_users = UserReader.getFollowUsers(req_user_name, full=True)
-            UserWriter.recordUserConnections(req_user_name, *full_of_follow_users)
-            result = UserAnalyzer.analyzeUserConnections(*full_of_follow_users)
-            return UserAnalyzer.generateUserAnalyzedHTML(result)
-        return redirect(url_for('loginfail')) 
-    except:
-        return redirect(url_for('loginfail')) 
+@app.route('/analyze/<user_name>', methods=['GET', 'POST'])
+def analyze(user_name):
+    if request.method == 'GET':
+        full_of_follow_users = UserReader.getFollowUsers(user_name, full=True)
+        UserWriter.recordUserConnections(user_name, *full_of_follow_users)
+        return UserAnalyzer.generateUserAnalyzedHTML(UserAnalyzer.analyzeUserConnections(*full_of_follow_users))
+    elif request.method == 'POST':
+        req_user_name = request.form['user_name'].strip()
+        try:
+            if UserReader.getUserAccountsDB()[req_user_name] == request.form['password'].strip():
+                return redirect(url_for('analyze', user_name=req_user_name))
+            return redirect(url_for('fail', action='login')) 
+        except:
+            return redirect(url_for('fail', action='login')) 
 
-@app.route('/loginfail', methods=['GET'])
-def loginfail():
-    return render_template('retry_login.html')
+@app.route('/fail/<action>', methods=['GET'])
+def fail(action):
+    return render_template(f'retry_{action}.html')
+
+@app.route('/success/<action>', methods=['GET'])
+def success(action):
+    return render_template(f'success_{action}.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=12345)
