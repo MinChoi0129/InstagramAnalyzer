a385ec583fb46fc146848c1c2879d0bf64960629
MinJae Lee <wjdchs0129@gnu.ac.kr>
2023-01-21 02:07:12 +0900

    update files

--- User.py
+++ User.py
@@ -1,4 +1,3 @@
-from flask import request
 from pathlib import Path
 from os import mkdir
 
@@ -19,7 +18,7 @@ class UserReader:
             return user_accounts
 
     @staticmethod
-    def getFollowUsers(user_name, data=None, time_mode=None, relative_mode=None, full=False):
+    def getFollowUsers(user_name, data):
         f = open(PATH_USER_DB + user_name + '/old_followers.dat', mode = 'r')
         g = open(PATH_USER_DB + user_name + '/old_followings.dat', mode = 'r')
         
@@ -30,29 +29,28 @@ class UserReader:
 
         f.close(); g.close()
         
-        if full: return a, b, c, d
-        
-        if time_mode == 'old':
-            if relative_mode == 'follower': return a
-            elif relative_mode == 'following': return b
-            elif relative_mode == 'both': return a, b
-        elif time_mode == 'current':
-            if relative_mode == 'follower': return c
-            elif relative_mode == 'following': return d
-            elif relative_mode == 'both': return c, d
+        return a, b, c, d
 
 class UserWriter:
     @staticmethod
     def recordUserConnections(user_name, old_followers, old_followings, current_followers, current_followings):
         UserWriter.writeUserData(open(PATH_USER_DB + user_name + '/old_followers.dat', mode = 'a'), current_followers, old_followers)
         UserWriter.writeUserData(open(PATH_USER_DB + user_name + '/old_followings.dat', mode = 'a'), current_followings, old_followings)
+        UserWriter.writeUserData(open(PATH_USER_DB + user_name + '/latest_followers.dat', mode = 'w'), current_followers, [], mode='DIRTY')
+        UserWriter.writeUserData(open(PATH_USER_DB + user_name + '/latest_followings.dat', mode = 'w'), current_followings, [], mode='DIRTY')
 
     @staticmethod
-    def writeUserData(file_obj, iterator, comparator):
-        for user in iterator:
-            if user not in comparator:
-                file_obj.write(user + '\n')
-        file_obj.close()
+    def writeUserData(file_obj, iterator, comparator, mode=None):
+        if mode:
+            for user in iterator:
+                if user not in comparator:
+                    file_obj.write(user + '님의 프로필 사진\r\n')
+            file_obj.close()
+        else:
+            for user in iterator:
+                if user not in comparator:
+                    file_obj.write(user + '\r\n')
+            file_obj.close()
 
     @staticmethod
     def writeNewDirectoryAndFiles(req_user_name, req_password):
@@ -60,6 +58,8 @@ class UserWriter:
         mkdir(PATH_USER_DB + req_user_name)
         Path(PATH_USER_DB + req_user_name + '/old_followings.dat').touch()
         Path(PATH_USER_DB + req_user_name + '/old_followers.dat').touch()
+        Path(PATH_USER_DB + req_user_name + '/latest_followings.dat').touch()
+        Path(PATH_USER_DB + req_user_name + '/latest_followers.dat').touch()
 
 class UserAnalyzer:
     @staticmethod
@@ -68,6 +68,7 @@ class UserAnalyzer:
         current_bi_follows = current_followings & current_followers # 지금 맞팔중인 유저들
         nickname_change_bi_follow = current_bi_follows - old_bi_follows # 아이디 변경한 유저
         un_bi_followed_by_others = old_bi_follows - current_bi_follows # 테스터가 맞팔해제 당하게 한 유저들
+        just_unfollowed_by_others = old_followers - current_followers - un_bi_followed_by_others # 상대방 혼자 팔로우 하다가 혼자 팔로우 해제한 유저
         current_uni_follows_by_you = current_followings - current_followers # 지금 테스터만 팔로우 중인 유저들
 
         return [
@@ -75,12 +76,13 @@ class UserAnalyzer:
             current_followings, # 지금 팔로잉들
             current_bi_follows, # 지금 맞팔중인 유저들
             un_bi_followed_by_others, # 테스터가 맞팔해제 당하게 한 유저들 or 아이디 변경한 유저
+            just_unfollowed_by_others, # 상대방만 혼자 팔로우 하다가 그 사람 혼자 팔로우 해제한 유저
+            nickname_change_bi_follow, # 테스터를 새로 팔로우 하거나 아이디 변경한 유저
             current_uni_follows_by_you, # 지금 테스터만 팔로우 중인 유저들
-            nickname_change_bi_follow # 아이디 변경한 유저
         ]
     
     @staticmethod
-    def generateUserAnalyzedHTML(result):
+    def generateUserAnalyzedHTML(req_user_name, result):
         txt = ""
         
         with open('./templates/analyzed_result.html', mode='r', encoding='utf-8') as f:
@@ -90,26 +92,52 @@ class UserAnalyzer:
                 if line.startswith('</html>'): break
 
                 if line.startswith('<div class="center_frame">'):
-                    txt += f"<h2>팔로워 : {len(result[0])}, 팔로잉 : {len(result[1])}</h2>"
-                    txt += f"<h2>맞팔로잉 : {len(result[2])}</h2>"
+                    # 0, 1, 2
+                    txt += f"<h2>팔로워 : {len(result[0])}, 팔로잉 : {len(result[1])}</h2><h2>맞팔로잉 : {len(result[2])}</h2>"
 
-                    for user_set in [result[3], result[5], result[4]]:
-                        if user_set and user_set == result[3]: 
-                            txt += "<h3>맞팔로우 취소한 유저(※ 주의 ※ : 아이디 변경한 유저일 수 있음)</h3>"
-                            txt += "<div class='show_box' style='border: 4px dotted yellow; width: 400px;'>"
-                            for user in user_set:
-                                txt += f'<div><p><a href="https://www.instagram.com/{user}/" target="_blank">{user}</a></p>'
-                                txt += '<button onclick="remove(this)">삭제</button></div>'
-                        elif user_set and user_set == result[4]: 
-                            txt += f"<h3>고객님만 팔로우 중인 유저 목록({len(user_set)}명)</h3>"
-                            txt += "<div class='show_box' style='border: 4px dotted yellow; width: 400px;'>"
-                            for user in user_set:
-                                txt += f'<p><a target="_blank" href="https://www.instagram.com/{user}/">{user}</a></p>'
-                        elif user_set and user_set == result[5]: 
-                            txt += "<h3>아이디를 변경한 유저 목록(※ 주의 ※ : 이 항목은 이번만 나타남)</h3>"
-                            txt += "<div class='show_box' style='border: 4px dotted yellow; width: 400px;'>"
-                            for user in user_set:
-                                txt += f'<p><a target="_blank" href="https://www.instagram.com/{user}/">{user}</a></p>'
+                    for user_set in result[3:]:
+                        if user_set:
 
-                        txt += "</div>"
+                            # 3
+                            if user_set == result[3]: 
+                                txt += "<h3>맞팔로우 취소한 유저(※주의※ : 아이디를 바꾼 유저일 수 있음)</h3><div class='show_box'>"
+                                for user in user_set:
+                                    txt += f'<form method="post" action="/removeUserFromUnBiFollowList">'
+                                    txt += f'   <p><a href="https://www.instagram.com/{user}/" target="_blank">{user}</a></p>'
+                                    txt += f'   <input type="hidden" name="req_user_name" value="{req_user_name}" />'
+                                    txt += f'   <input type="hidden" name="target_name" value="{user}" />'
+                                    txt += f'   <input type="submit" value="삭제" />'
+                                    txt += f'</form>'
+
+                            # 4, 5, 6
+                            elif user_set == result[4]:
+                                txt += "<h3>상대방 혼자 팔로우 하다가 혼자 팔로우 해제한 유저</h3><div class='show_box'>"
+                                for user in user_set:
+                                    txt += f'<form method="post" action="/removeUserFromUnBiFollowList">'
+                                    txt += f'   <p><a href="https://www.instagram.com/{user}/" target="_blank">{user}</a></p>'
+                                    txt += f'   <input type="hidden" name="req_user_name" value="{req_user_name}" />'
+                                    txt += f'   <input type="hidden" name="target_name" value="{user}" />'
+                                    txt += f'   <input type="submit" value="삭제" />'
+                                    txt += f'</form>'
+
+
+                            elif user_set == result[5]: txt += "<h3>회원님을 새로 팔로우 하거나 아이디를 바꾼 유저 목록(※주의※ : 이 항목은 이번만 나타남)</h3><div class='show_box'>"
+                            elif user_set == result[6]: txt += f"<h3>회원님만 팔로우 중인 유저({len(user_set)}명)</h3><div class='show_box'>"
+                            if user_set in result[5:]:
+                                for user in user_set: txt += f'<p><a target="_blank" href="https://www.instagram.com/{user}/">{user}</a></p>'    
+
+                            # close for class: show_box
+                            txt += "</div>" 
+
+
+
+                            '''
+                            수정필요사항
+                            result[5]: 첫 회원가입시에 나오면 안됨
+                            removeUserFromUnBiFollowList 이름 바꾸기 bi가 아닐 수 있음
+                            코드 긴 것 리팩토링
+                            불필요 리팩토링 제거
+                            삭제버튼 css 재설정
+                            
+                            '''
             return txt
--- app.py
+++ app.py
@@ -23,9 +23,9 @@ def analyze():
     try:
         if UserReader.getUserAccountsDB()[req_user_name] == request.form['password'].strip():
             data = request.form
-            full_of_follow_users = UserReader.getFollowUsers(req_user_name, data, full=True)
+            full_of_follow_users = UserReader.getFollowUsers(req_user_name, data)
             UserWriter.recordUserConnections(req_user_name, *full_of_follow_users)
-            return UserAnalyzer.generateUserAnalyzedHTML(UserAnalyzer.analyzeUserConnections(*full_of_follow_users))
+            return UserAnalyzer.generateUserAnalyzedHTML(req_user_name, UserAnalyzer.analyzeUserConnections(*full_of_follow_users))
         return redirect(url_for('fail', action='login')) 
     except:
         return redirect(url_for('fail', action='login')) 
@@ -42,5 +42,26 @@ def success(action):
 def how_to_use():
     return render_template('how_to_use.html')
 
+@app.route('/removeUserFromUnBiFollowList', methods=['POST'])
+def removeUserFromUnBiFollowList():
+    req_user_name, target_name = request.form['req_user_name'], request.form['target_name']
+
+    F_PATH = PATH_USER_DB + req_user_name + '/old_followers.dat'
+    G_PATH = PATH_USER_DB + req_user_name + '/old_followings.dat'
+
+    with open(F_PATH, 'r', encoding='utf-8') as f, open(G_PATH, 'r', encoding='utf-8') as g:
+        new_f = ''.join([user for user in f.readlines() if user.strip() != target_name])
+        new_g = ''.join([user for user in g.readlines() if user.strip() != target_name])
+    with open(F_PATH, 'w', encoding='utf-8') as f, open(G_PATH, 'w', encoding='utf-8') as g:
+        f.write(new_f); g.write(new_g);
+
+
+    with open(PATH_USER_DB + req_user_name + '/latest_followers.dat', 'r', encoding='utf-8') as f, \
+         open(PATH_USER_DB + req_user_name + '/latest_followings.dat', 'r', encoding='utf-8') as g:
+        
+        data = {'followers': '\r\n'.join(f.readlines()), 'followings': '\r\n'.join(g.readlines())}
+        full_of_follow_users = UserReader.getFollowUsers(req_user_name, data)
+        return UserAnalyzer.generateUserAnalyzedHTML(req_user_name, UserAnalyzer.analyzeUserConnections(*full_of_follow_users))
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=12345)
