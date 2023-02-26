1a02391dd494ea1d3fcbb8d7706679e3aac390b4
MinJae Lee <wjdchs0129@gnu.ac.kr>
2023-02-26 17:55:17 +0900

    데이터 베이스 기능 추가

--- User.py
+++ User.py
@@ -1,78 +1,94 @@
-from pathlib import Path
-from os import mkdir
-from difflib import SequenceMatcher
+import pymysql
+from admin import password
 
-PATH_USER_DB = './static/user_db/'
+conn = pymysql.connect(host='127.0.0.1', user='root', password=password, db='INSTA_DB', charset='utf8', port=3306)
+cur = conn.cursor()
+
+try:
+    cur.execute('CREATE TABLE user \
+                (USERNAME CHAR(20) NOT NULL, \
+                ENC_PASSWORD CHAR(120) NOT NULL, \
+                OLD_FOLLOWERS MEDIUMTEXT, \
+                OLD_FOLLOWINGS MEDIUMTEXT, \
+                LATEST_FOLLOWERS MEDIUMTEXT, \
+                LATEST_FOLLOWINGS MEDIUMTEXT) \
+    ;')
+    conn.commit()
+except Exception as e:
+    print(e)
+    pass
+
 HOST, PORT = '0.0.0.0', 12345
+
 
 class UserReader:
     @staticmethod
-    def getUserList(raw_data):
+    def getUserListFromRawData(raw_data): # CLEAR
         return [string[:string.find('님')].strip() for string in raw_data.split('\r\n') if "프로필 사진" in string]
 
     @staticmethod
-    def getUserAccountsDB():
-        with open(PATH_USER_DB + 'user_accounts.csv', mode='r') as f:
-            user_accounts = {}
-            for line in f.readlines():
-                user_name, password = line.split(',')
-                user_accounts[user_name.strip()] = password.strip()
-            return user_accounts
+    def getUserAccountsDB(): # CLEAR
+        cur.execute("SELECT USERNAME, ENC_PASSWORD FROM user;")
+        conn.commit()
+        return {user_name: enc_password for user_name, enc_password in cur.fetchall()}
 
     @staticmethod
-    def getFollowUsers(user_name, data):
-        with open(PATH_USER_DB + user_name + '/old_followers.dat', mode = 'r') as f, open(PATH_USER_DB + user_name + '/old_followings.dat', mode = 'r') as g:
-            a, b = {user.strip() for user in f.readlines()}, {user.strip() for user in g.readlines()}
-            c, d = set(UserReader.getUserList(data['followers'])), set(UserReader.getUserList(data['followings']))
-        return a, b, c, d
+    def getFollowUsers(user_name, data=None): # CLEAR
+        cur.execute(f'SELECT OLD_FOLLOWERS, OLD_FOLLOWINGS FROM user WHERE USERNAME="{user_name}";')
+        conn.commit()
+        fetched_data = cur.fetchall()[0]
+        a, b = set(), set()
+        if fetched_data[0] != None: a = set(fetched_data[0].split(','))
+        if fetched_data[1] != None: b = set(fetched_data[1].split(','))
+
+        if not data: return a, b
+        cur.execute(f'SELECT LATEST_FOLLOWERS, LATEST_FOLLOWINGS FROM user WHERE USERNAME="{user_name}";')
+        conn.commit()
+        fetched_data = cur.fetchall()[0]
+        a, b = fetched_data[0].split(','), fetched_data[1].split(',')
+        c, d = UserReader.getUserListFromRawData(data['followers']), UserReader.getUserListFromRawData(data['followings'])
+        return set(a), set(b), set(c), set(d)
     
-    @staticmethod
-    def getUserPaths(req_user_name):
-        return [PATH_USER_DB + req_user_name + detail for detail in ['/old_followers.dat', '/old_followings.dat', '/latest_followers.dat', '/latest_followings.dat']]
-
 class UserWriter:
     @staticmethod
-    def recordUserConnections(user_name, old_followers, old_followings, current_followers, current_followings):
-        UserWriter.writeUserData(open(PATH_USER_DB + user_name + '/old_followers.dat', mode = 'a'), current_followers, old_followers)
-        UserWriter.writeUserData(open(PATH_USER_DB + user_name + '/old_followings.dat', mode = 'a'), current_followings, old_followings)
-        UserWriter.writeUserData(open(PATH_USER_DB + user_name + '/latest_followers.dat', mode = 'w'), current_followers, [], long=True)
-        UserWriter.writeUserData(open(PATH_USER_DB + user_name + '/latest_followings.dat', mode = 'w'), current_followings, [], long=True)
+    def recordUserConnections(user_name, mode, a=None, b=None, c=None, d=None):
+        cmd = ""
+        if mode == 'update_old_relationships': # a, b
+            cmd = f'UPDATE user SET \
+                    OLD_FOLLOWERS = "{a}", OLD_FOLLOWINGS = "{b}" \
+                    WHERE USERNAME = "{user_name}";'
+            
+        elif mode == 'new_analyze': # a, b, c, d
+            old_followers = ','.join(a)
+            old_followings = ','.join(b)
+            latest_followers = ','.join(c)
+            latest_followings = ','.join(d)
+            cmd = f'UPDATE user SET \
+                    OLD_FOLLOWERS = "{old_followers}", OLD_FOLLOWINGS = "{old_followings}", \
+                    LATEST_FOLLOWERS = "{latest_followers}", LATEST_FOLLOWINGS = "{latest_followings}" \
+                    WHERE USERNAME = "{user_name}";'
+            
+        cur.execute(cmd)
+        conn.commit()
 
     @staticmethod
-    def writeUserData(file_obj, iterator, comparator, long=None):
-        tail = '님의 프로필 사진\r\n' if long else '\r\n'
-        for user in iterator:
-            if user not in comparator:
-                file_obj.write(user + tail)
-        file_obj.close()
-
-    @staticmethod
-    def writeNewDirectoryAndFiles(req_user_name, req_password):
-        with open(PATH_USER_DB + 'user_accounts.csv', mode='a') as f: f.write(req_user_name + ',' + req_password + '\n')
-        mkdir(PATH_USER_DB + req_user_name)
-        for path in UserReader.getUserPaths(req_user_name):
-            Path(path).touch()
-
+    def addUserIntoDatabase(req_user_name, req_password): # CLEAR
+        cur.execute(f'INSERT INTO user \
+                    (USERNAME, ENC_PASSWORD, OLD_FOLLOWINGS, OLD_FOLLOWERS, LATEST_FOLLOWINGS, LATEST_FOLLOWERS) \
+                    VALUES ("{req_user_name}", "{str(req_password)}", "", "", "", "")')
+        conn.commit()
+    
 class UserAnalyzer:
     @staticmethod
-    def getSimilarity(user_list: set[str], target: str) -> tuple[str, float]: 
-        stringHandle = lambda a: ''.join(''.join(a.strip().split('_')).split('.')).lower()
-        answer_bytes = list(bytes(stringHandle(target), 'utf-8'))
-        for input_string in map(stringHandle, user_list):
-            input_bytes = list(bytes(input_string, 'utf-8'))
-            similarity_percent = SequenceMatcher(None, answer_bytes, input_bytes).ratio() * 100
-            if similarity_percent >= 55:
-                return input_string, similarity_percent
-        return None, None
-
-    @staticmethod
-    def analyzeUserConnections(m, n, p, q):
+    def analyzeUserConnections(m, n, p, q): # 조건 재분석 필요
         old_bi_follows = m & n #  과거 맞팔했던 유저
         current_bi_follows = p & q #  지금 맞팔중인 유저
         current_uni_follows_by_you = q - p #  지금 테스터만 팔로우 중인 유저
 
+        new_followers = p - m
         nickname_changed = (p | q) - (m | n) #  아이디 변경한 유저
-        new_followers = p - m - nickname_changed # 테스터를 새로 팔로우 하는 유저(초기 분석은 포함하지 않도록 뒤에서 조건 처리 필요)
+        new_followers -= nickname_changed # 테스터를 새로 팔로우 하는 유저(초기 분석은 포함하지 않도록 뒤에서 조건 처리 필요)
+
         disabled = (m | n) - (p | q) # 계정 비활성화한 유저
         uni_un_followed_by_others = (m - n) - (p - q) - disabled - nickname_changed #  상대방만 혼자 팔로우 하다가 그 사람 혼자 팔로우 해제한 유저(비활, 아이디 변경은 포함하지 않음)
         un_bi_followed_by_others = (m & n) - (p & q) - disabled - nickname_changed #  테스터가 맞팔해제 당하게 한 유저들(비활, 아이디 변경은 포함하지 않음)
@@ -85,11 +101,12 @@ class UserAnalyzer:
             q, # ------------------------------------------  3 지금 팔로잉
             old_bi_follows, # -----------------------------  4 과거 맞팔했던 유저
             current_bi_follows, # -------------------------  5 지금 맞팔중인 유저
+
             new_followers, # ------------------------------  6 테스터를 새로 팔로우 하는 유저
             un_bi_followed_by_others, # -------------------  7 테스터가 맞팔해제 당하게 한 유저들(비활, 아이디 변경은 포함하지 않음)
             disabled, # -----------------------------------  8 계정 비활성화한 유저
             uni_un_followed_by_others, # ------------------  9 상대방만 혼자 팔로우 하다가 그 사람 혼자 팔로우 해제한 유저
-            nickname_changed, # --------------------------- 10 아이디 변경한 유저
+            nickname_changed, # --------------------------- 10 아이디 변경한 유저 or 계정 다시 활성화한 유저
             current_uni_follows_by_you, # ----------------- 11 지금 테스터만 팔로우 중인 유저
         ]
         
@@ -98,12 +115,10 @@ class UserAnalyzer:
             except: continue
         
         return return_targets
-            
-    
+
     @staticmethod
-    def generateUserAnalyzedHTML(result):
-        txt = ""
-        
+    def generateUserAnalyzedHTML(result): # 조건 재분석 필요
+        txt = ""      
         with open('./templates/analyzed_result.html', mode='r', encoding='utf-8') as f:
             while True:
                 line = f.readline().strip()
@@ -111,8 +126,6 @@ class UserAnalyzer:
                 if line.startswith('</html>'): break
                 if line.startswith('<div class="center_frame">'):
                     txt += f"<h2>팔로워 : {len(result[2])}, 팔로잉 : {len(result[3])}</h2><h2>맞팔로잉 : {len(result[5])}</h2>"
-
-                    
                     
                     if result[6] and (result[0] or result[1]):
                         txt += f"<h3>회원님을 새로 팔로우하는 유저({len(result[6])})</h3><div class='show_box'>"
@@ -136,7 +149,8 @@ class UserAnalyzer:
                             txt += f'<p><a target="_blank" href="https://www.instagram.com/{user}/">{user}</a></p>'
                         txt += "</div>" 
                     if result[10] and (result[0] or result[1]):
-                        txt += f"<h3>인스타그램 아이디를 변경한 유저({len(result[10])})</h3><div class='show_box'>"
+                        txt += f"<h3>인스타그램 아이디를 변경한 유저({len(result[10])})</h3>"
+                        txt += f"<h4>계정을 다시 활성화 한 유저일 수 있습니다.</h4><div class='show_box'>"
                         for user in result[10]:
                             txt += f'<p><a target="_blank" href="https://www.instagram.com/{user}/">{user}</a></p>'
                         txt += "</div>" 
@@ -147,21 +161,3 @@ class UserAnalyzer:
                         txt += "</div>" 
                     
             return txt
-
-
-
-
-
-            # similar_user_name, similarity = UserAnalyzer.getSimilarity(result[4], user)
-            # show_user_name =  + '(이전 아이디 : ' +  + ') - ' + str(similarity) + '%' if similar_user_name else user
-
-            # for user in result[7]:
-            #     txt += f'<form method="post" action="/analyze">'
-            #     txt += f'   <p><a href="https://www.instagram.com/{user}/" target="_blank">{user}</a></p>'
-            #     txt += f'   <input type="hidden" name="req_user_name" value="{req_user_name}" />'
-            #     txt += f'   <input type="hidden" name="target_name" value="{user}" />'
-            #     txt += f'   <input type="submit" value="삭제" />'
-            #     txt += f'</form>'
-
-            # for user in result[]:
-            #     txt += f'<p><a target="_blank" href="https://www.instagram.com/{user}/">{user}</a></p>'
--- main.py
+++ main.py
@@ -5,52 +5,54 @@ from bcrypt import checkpw, hashpw, gensalt
 app = Flask(__name__)
 
 # Normal Functions
-def removeFromUnfollowList(req_user_name, target_name):
-    OLD_F, OLD_G, NEW_F, NEW_G = R.getUserPaths(req_user_name)
-    with open(OLD_F, 'r', encoding='utf-8') as f, open(OLD_G, 'r', encoding='utf-8') as g: data = [''.join([u for u in h.readlines() if u.strip() != target_name]) for h in [f, g]]
-    with open(OLD_F, 'w', encoding='utf-8') as f, open(OLD_G, 'w', encoding='utf-8') as g: f.write(data[0]); g.write(data[1])
-    with open(NEW_F, 'r', encoding='utf-8') as f, open(NEW_G, 'r', encoding='utf-8') as g:
-        raw_data = {'followers': '\r\n'.join(f.readlines()), 'followings': '\r\n'.join(g.readlines())}
-        full_of_follow_users = R.getFollowUsers(req_user_name, raw_data)
-        return A.generateUserAnalyzedHTML(A.analyzeUserConnections(*full_of_follow_users))
+def removeFromOldRelationship(req_user_name, target_name):
+    old_followers, old_followings = R.getFollowUsers(req_user_name)
+    old_followers.remove(target_name)
+    old_followings.remove(target_name)
+    a, b = ','.join(old_followers), ','.join(old_followings)
+    W.recordUserConnections(req_user_name, a = a, b = b, mode='update_old_relationships')
 
-def signupResponse(req_user_name, req_password):
+def signupResponse(req_user_name, req_password): # CLEAR
     if req_user_name in R.getUserAccountsDB(): return redirect(url_for('fail', action='signup'))
-    W.writeNewDirectoryAndFiles(req_user_name, hashpw(req_password.encode('UTF-8'), gensalt()).hex())
+    W.addUserIntoDatabase(req_user_name, hashpw(req_password.encode('UTF-8'), gensalt()).hex())
     return redirect(url_for('success', action='signup'))
 
 # Routers
 @app.route('/')
-def main():
+def main(): # CLEAR
     return render_template('index.html')
 
 @app.route('/fail/<action>', methods=['GET'])
-def fail(action):
+def fail(action): # CLEAR
     return render_template(f'retry_{action}.html')
 
 @app.route('/success/<action>', methods=['GET'])
-def success(action):
+def success(action): # CLEAR
     return render_template(f'success_{action}.html')
 
 @app.route('/instruction')
-def instruction():
+def instruction(): # CLEAR, 사진 변경?
     return render_template('instruction.html')
 
 @app.route('/signup', methods = ['GET', 'POST'])
-def signup():
-    return render_template('signup.html') if request.method == 'GET' else signupResponse(request.form['user_name'].strip(), request.form['password'].strip())
+def signup(): # CLEAR
+    return render_template('signup.html') if request.method == 'GET' else signupResponse(request.form['user_name'], request.form['password'])
     
 @app.route('/analyze', methods=['POST'])
 def analyze():
-    try: return removeFromUnfollowList(request.form['req_user_name'], request.form['target_name'])
+    try:
+        return removeFromOldRelationship(request.form['req_user_name'], request.form['target_name'])
     except:
         try:
-            req_user_name = request.form['user_name'].strip()
-            req_password = request.form['password'].strip().encode('UTF-8')
+            # 계정 검증
+            req_user_name = request.form['user_name']
+            req_password = request.form['password'].encode('UTF-8')
             db_password = bytes.fromhex(R.getUserAccountsDB()[req_user_name])
             if not checkpw(req_password, db_password): return redirect(url_for('fail', action='login'))
-            full_of_follow_users = R.getFollowUsers(req_user_name, request.form)
-            W.recordUserConnections(req_user_name, *full_of_follow_users)
+
+            # DB 및 form에서 정보 받기
+            full_of_follow_users = R.getFollowUsers(req_user_name, request.form) # a, b, c, d
+            W.recordUserConnections(req_user_name, 'new_analyze', *full_of_follow_users)
             return A.generateUserAnalyzedHTML(A.analyzeUserConnections(*full_of_follow_users))
         except KeyError: return redirect(url_for('fail', action='login')) 
         except Exception as e: return str(e)
